4c8dc64e5370b77081d6f265290604ac05337e52
Ricardo Rodriguez <ricardovelero@gmail.com>
2023-10-27 20:15:56 +0200

    Handle errors and not found

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -30,32 +30,39 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
-  await sql`
+  try {
+    await sql`
     INSERT INTO invoices(customer_id, amount, status, date)
     VALUES(${customerId}, ${amountInCents}, ${status}, ${date})
   `;
-
+  } catch (error) {
+    console.log(error);
+    return {
+      message: 'Database Error: Failed to Create Invoice.',
+    };
+  }
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 const UpdateInvoice = InvoiceSchema.omit({ date: true });
 export async function updateInvoice(formData: FormData) {
-  const { id, customerId, amount, status } = UpdateInvoice.parse({
-    id: formData.get('id'),
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
-  });
-
+  const { id, customerId, amount, status } = UpdateInvoice.parse(
+    Object.fromEntries(formData.entries())
+  );
   const amountInCents = amount * 100;
-
-  await sql`
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
-
+  } catch (error) {
+    console.log(error);
+    return {
+      message: 'Database Error: Failed to Update Invoice.',
+    };
+  }
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
@@ -64,6 +71,14 @@ const DeleteInvoice = InvoiceSchema.pick({ id: true });
 
 export async function deleteInvoice(formData: FormData) {
   const id = formData.get('id')?.toString();
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+  } catch (error) {
+    console.log(error);
+    return {
+      message: 'Database Error: Failed to Delete Invoice.',
+    };
+  }
+
   revalidatePath('/dashboard/invoices');
 }
